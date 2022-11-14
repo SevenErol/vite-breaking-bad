3f9d8474ac25955ac10af9b9876642d6161e26ba
SevenErol <lorenzo.maggi1998@gmail.com>
2022-11-14 14:53:08 +0100

    commit site header logo and title

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,22 @@
+<script>
+
+export default {
+    name: "SiteHeader"
+}
+
+</script>
+
+<template>
+    <div class="header_container">
+
+        <img src="../assets/img/Logo-Breaking-Bad-PNG-Photos.png" alt="">
+        <h2>Breaking Bad API</h2>
+
+    </div>
+
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { reactive } from "vue"
+
+export const store = reactive({
+    API_URL: "https://www.breakingbadapi.com/api/characters",
+    characters: null,
+    name: null,
+    info: null
+})
